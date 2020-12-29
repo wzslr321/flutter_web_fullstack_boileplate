db9da2fd98d172d0128ddf4981b5cb7607d1268a
Wiktor ZAając <wiktor.zajac888@gmail.com>
2020-12-29 02:39:37 +0100

    tasks fetch with http and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 import './providers/tasks_provider.dart';
 
 import './screens/home_screen/home_screen.dart';
--- lib/screens/tasks_screen/tasks_screen.dart
+++ lib/screens/tasks_screen/tasks_screen.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class TasksScreen extends StatelessWidget {
+import '../../providers/tasks_provider.dart';
+
+class TasksScreen extends StatefulWidget {
   static const routeName = '/tasks';
+
+  @override
+  _TasksScreenState createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  var _isInit = true;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_isInit) {
+      Provider.of<TasksProvider>(context).fetchTasks();
+    }
+    _isInit = false;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Tasks screen"),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: const Text("Tasks screen"),
+        ),
+        body: Container(
+          child: Consumer<TasksProvider>(
+            builder: (_, tasks, __) {
+              return tasks.tasks == null
+                  ? Text("Loading posts")
+                  : Text("${tasks.tasks['title']}");
+            },
+          ),
+        ));
   }
 }
