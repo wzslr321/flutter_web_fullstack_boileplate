180ba3c85adeb7b16746c10599bf8990acc09ad5
Wiktor ZAając <wiktor.zajac888@gmail.com>
2021-02-02 20:02:33 +0100

    announcements screen http fetch

--- app/lib/screens/announcements_screen/announcements_screen.dart
+++ app/lib/screens/announcements_screen/announcements_screen.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod/riverpod.dart';
 
-class AnnouncementsScreen extends HookWidget {
+import '../../providers/announcements_state.dart';
+
+class AnnouncementsScreen extends ConsumerWidget {
   const AnnouncementsScreen({Key key}) : super(key: key);
 
   static const routeName = '/announcements';
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: const Scaffold(
-        body: Text('gonna be implemented'),
+  Widget build(BuildContext context, T Function<T>(ProviderBase<Object,T> provider) watch) {
+    final announcements = watch(announcementStateFuture);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FGGP'),
       ),
+      body: announcements.when(
+          loading: () => const Center(child: CircularProgressIndicator()),
+          error: (error, stack) => Center(child: Text('$error')),
+          data: (value) {
+            return ListView.builder(
+              itemCount: value.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(value[index].title),
+                );
+              },
+            );
+          }),
     );
   }
 }
